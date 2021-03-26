b4fc42da877e0167772de4ab10b02769e596009d
Hamed Adefuwa <62181704+hadefuwa@users.noreply.github.com>
2021-03-26 17:28:00 +0000

    Add files via upload

---
+++ My_Temp_Sensor_Code.ino
@@ -0,0 +1,99 @@
+
+// Robo India Tutorial 
+// Simple code upload the tempeature and humidity data using thingspeak.com
+// Hardware: NodeMCU,DHT11
+
+#include <DHT.h>  // Including library for dht
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>   // Include the Wi-Fi-Multi library
+
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+ 
+String apiKey = "*********";     //  Enter your Write API key from ThingSpeak
+
+const char *ssid =  "VM2279333";    // replace with your wifi ssid and wpa2 key
+const char *pass =  "*********";
+const char* server = "api.thingspeak.com";
+
+#define DHTPIN D6          //pin where the dht11 is connected
+ 
+DHT dht(DHTPIN, DHT11);
+
+WiFiClient client;
+ 
+void setup() 
+{
+       Serial.begin(115200);
+       delay(10);
+       dht.begin();
+ 
+  wifiMulti.addAP("VM2279333", "*********");    //Home
+  wifiMulti.addAP("VM5062639", "*********");    //Office
+  wifiMulti.addAP("VM2279333 (2)", "*********");//Home 2
+  wifiMulti.addAP("Hamed", "*********");        //Mobile Hotspot
+
+
+  Serial.println("Connecting ...");
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+    delay(1000);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+
+ 
+}
+ 
+void loop() 
+{
+  
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      
+              if (isnan(h) || isnan(t)) 
+                 {
+                     Serial.println("Failed to read from DHT sensor!");
+                      return;
+                 }
+
+                         if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+                      {  
+                            
+                             String postStr = apiKey;
+                             postStr +="&field1=";
+                             postStr += String(t);
+                             postStr +="&field2=";
+                             postStr += String(h);
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                             Serial.print("Temperature: ");
+                             Serial.print(t);
+                             Serial.print(" degrees Celcius, Humidity: ");
+                             Serial.print(h);
+                             Serial.println("%.");
+                        }
+          client.stop();
+ 
+          Serial.println("Waiting...");
+  //ESP.deepSleep(600e6); 
+  //ESP.deepSleep(300e6); 
+  ESP.deepSleep(200e6); 
+
+  // thingspeak needs minimum 15 sec delay between updates
+  //delay(600000);
+}
